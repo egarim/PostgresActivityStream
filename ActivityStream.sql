ef5d41370e9f5b802c738e21971c2e76f22b7d7a
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-03-14 07:41:07 +0300

    rename the objects with the prefix as_
    closes #4

--- ActivityStream.sql
+++ ActivityStream.sql
@@ -3,7 +3,7 @@
 
 CREATE EXTENSION IF NOT EXISTS postgis; -- Enable PostGIS extension
 
-CREATE TABLE objectstorage (
+CREATE TABLE as_objectstorage (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     latitude DECIMAL(9,6) NOT NULL,
     longitude DECIMAL(9,6) NOT NULL,
@@ -14,20 +14,20 @@ CREATE TABLE objectstorage (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE OR REPLACE FUNCTION update_location() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION as_update_location() RETURNS TRIGGER AS $$
 BEGIN
     NEW.location := ST_SetSRID(ST_MakePoint(NEW.longitude, NEW.latitude), 4326);
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_location
+CREATE TRIGGER as_set_location
     BEFORE INSERT OR UPDATE
-    ON objectstorage
+    ON as_objectstorage
     FOR EACH ROW
-    EXECUTE FUNCTION update_location();
+    EXECUTE FUNCTION as_update_location();
    
-CREATE OR REPLACE FUNCTION upsert_objectstorage(
+CREATE OR REPLACE FUNCTION as_upsert_objectstorage(
     p_id UUID, 
     p_latitude DECIMAL(9,6), 
     p_longitude DECIMAL(9,6),
@@ -36,7 +36,7 @@ CREATE OR REPLACE FUNCTION upsert_objectstorage(
 ) RETURNS VOID AS $$
 BEGIN
     -- Try to update the existing row
-    UPDATE objectstorage SET
+    UPDATE as_objectstorage SET
         latitude = p_latitude,
         longitude = p_longitude,
         location = ST_SetSRID(ST_MakePoint(p_longitude, p_latitude), 4326),
@@ -47,19 +47,19 @@ BEGIN
     
     -- If no row was updated, insert a new one
     IF NOT FOUND THEN
-        INSERT INTO objectstorage (id, latitude, longitude, location, object_type, object_data)
+        INSERT INTO as_objectstorage (id, latitude, longitude, location, object_type, object_data)
         VALUES (p_id, p_latitude, p_longitude, ST_SetSRID(ST_MakePoint(p_longitude, p_latitude), 4326), p_object_type, p_object_data);
     END IF;
 END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE TABLE activity (
+CREATE TABLE as_activity (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     verb TEXT NOT NULL,
-    actor_id UUID NOT NULL REFERENCES objectstorage(id),
-    object_id UUID NOT NULL REFERENCES objectstorage(id),
-    target_id UUID REFERENCES objectstorage(id),
+    actor_id UUID NOT NULL REFERENCES as_objectstorage(id),
+    object_id UUID NOT NULL REFERENCES as_objectstorage(id),
+    target_id UUID REFERENCES as_objectstorage(id),
     latitude DECIMAL(9,6) NOT NULL,
     longitude DECIMAL(9,6) NOT NULL,
     location GEOMETRY(Point, 4326) NOT NULL,
@@ -67,21 +67,25 @@ CREATE TABLE activity (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE OR REPLACE FUNCTION update_activity_location() RETURNS TRIGGER AS $$
+
+
+CREATE OR REPLACE FUNCTION as_update_activity_location() RETURNS TRIGGER AS $$
 BEGIN
     NEW.location := ST_SetSRID(ST_MakePoint(NEW.longitude, NEW.latitude), 4326);
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_activity_location
+
+
+CREATE TRIGGER as_set_activity_location
     BEFORE INSERT OR UPDATE
-    ON activity
+    ON as_activity
     FOR EACH ROW
-    EXECUTE FUNCTION update_activity_location();
+    EXECUTE FUNCTION as_update_activity_location();
 
 
-CREATE OR REPLACE FUNCTION upsert_activity(
+CREATE OR REPLACE FUNCTION as_upsert_activity(
     p_id UUID,
     p_verb TEXT,
     p_actor_id UUID,
@@ -92,7 +96,7 @@ CREATE OR REPLACE FUNCTION upsert_activity(
 ) RETURNS VOID AS $$
 BEGIN
     -- Try to update the existing row
-    UPDATE activity SET
+    UPDATE as_activity SET
         verb = p_verb,
         actor_id = p_actor_id,
         object_id = p_object_id,
@@ -105,7 +109,7 @@ BEGIN
     
     -- If no row was updated, insert a new one
     IF NOT FOUND THEN
-        INSERT INTO activity (id, verb, actor_id, object_id, target_id, latitude, longitude, location)
+        INSERT INTO as_activity (id, verb, actor_id, object_id, target_id, latitude, longitude, location)
         VALUES (p_id, p_verb, p_actor_id, p_object_id, p_target_id, p_latitude, p_longitude, ST_SetSRID(ST_MakePoint(p_longitude, p_latitude), 4326));
     END IF;
 END;
@@ -113,19 +117,19 @@ $$ LANGUAGE plpgsql;
 
 
 ---
-ALTER TABLE activity ADD COLUMN self JSON;
+ALTER TABLE as_activity ADD COLUMN self JSON;
 
-CREATE OR REPLACE FUNCTION update_activity_self() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION as_update_activity_self() RETURNS TRIGGER AS $$
 BEGIN
     NEW.self = json_build_object(
         'id', NEW.id,
         'verb', NEW.verb,
         'actor_id',NEW.actor_id,
-        'actor', (SELECT object_data FROM objectstorage WHERE id = NEW.actor_id),
+        'actor', (SELECT object_data FROM as_objectstorage WHERE id = NEW.actor_id),
         'object_id',NEW.object_id,
-        'object', (SELECT object_data FROM objectstorage WHERE id = NEW.object_id),
+        'object', (SELECT object_data FROM as_objectstorage WHERE id = NEW.object_id),
         'target_id',NEW.target_id,
-        'target', (SELECT object_data FROM objectstorage WHERE id = NEW.target_id),
+        'target', (SELECT object_data FROM as_objectstorage WHERE id = NEW.target_id),
         'latitude', NEW.latitude,
         'longitude', NEW.longitude,
         'created_at', NEW.created_at,
@@ -135,12 +139,12 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER activity_self_trigger
-    BEFORE INSERT OR UPDATE ON activity
+CREATE TRIGGER as_activity_self_trigger
+    BEFORE INSERT OR UPDATE ON as_activity
     FOR EACH ROW
-    EXECUTE FUNCTION update_activity_self();
+    EXECUTE FUNCTION as_update_activity_self();
 
-CREATE OR REPLACE FUNCTION get_activities_by_distance_as_json(
+CREATE OR REPLACE FUNCTION as_get_activities_by_distance_as_json(
     p_lat NUMERIC,
     p_long NUMERIC,
     p_distance INTEGER,
@@ -155,7 +159,7 @@ BEGIN
     SELECT json_agg(a.self) INTO activities_json
     FROM (
         SELECT a.self
-        FROM activity a
+        FROM as_activity a
         WHERE ST_DWithin(location::geography, ST_SetSRID(ST_Point(p_long, p_lat), 4326)::geography, p_distance)
         ORDER BY created_at DESC
         LIMIT p_page_size
@@ -168,15 +172,15 @@ $$ LANGUAGE plpgsql;
 ---
 
 
-CREATE TABLE follow (
+CREATE TABLE as_follow (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    follower_id UUID NOT NULL REFERENCES objectstorage(id),
-    followee_id UUID NOT NULL REFERENCES objectstorage(id),
+    follower_id UUID NOT NULL REFERENCES as_objectstorage(id),
+    followee_id UUID NOT NULL REFERENCES as_objectstorage(id),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE OR REPLACE FUNCTION follow_user(
+CREATE OR REPLACE FUNCTION as_follow_user(
     p_follower_id UUID,
     p_followee_id UUID
 ) RETURNS VOID AS $$
@@ -184,37 +188,37 @@ BEGIN
     -- Check if a row with the same follower_id and followee_id values already exists
     IF EXISTS (
         SELECT 1
-        FROM follow
+        FROM as_follow
         WHERE follower_id = p_follower_id AND followee_id = p_followee_id
     ) THEN
         RETURN;
     END IF;
     
     -- If the row does not exist, insert a new row into the follow table
-    INSERT INTO follow (follower_id, followee_id)
+    INSERT INTO as_follow (follower_id, followee_id)
     VALUES (p_follower_id, p_followee_id);
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION unfollow_user(
+CREATE OR REPLACE FUNCTION as_unfollow_user(
     p_follower_id UUID,
     p_followee_id UUID
 ) RETURNS VOID AS $$
 BEGIN
     -- Delete the row from the follow table where the follower_id and followee_id match
-    DELETE FROM follow
+    DELETE FROM as_follow
     WHERE follower_id = p_follower_id AND followee_id = p_followee_id;
 END;
 $$ LANGUAGE plpgsql;
 
 --Activities
-CREATE OR REPLACE FUNCTION get_following_ids(p_user_id UUID)
+CREATE OR REPLACE FUNCTION as_get_following_ids(p_user_id UUID)
 RETURNS UUID[] AS $$
 DECLARE
   following_ids UUID[];
 BEGIN
   SELECT ARRAY_AGG(followee_id) INTO following_ids
-  FROM follow
+  FROM as_follow
   WHERE follower_id = p_user_id;
   
   RETURN following_ids;
@@ -222,7 +226,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION get_activities_by_followed_ids(
+CREATE OR REPLACE FUNCTION as_get_activities_by_followed_ids(
   p_page_number INTEGER,
   p_page_size INTEGER,
   p_followed_ids UUID[]
@@ -252,7 +256,7 @@ BEGIN
          a.longitude,
          a.created_at,
          a.updated_at
-  FROM activity a
+  FROM as_activity a
   WHERE a.actor_id = ANY(p_followed_ids)
   ORDER BY a.created_at DESC, a.id DESC
   LIMIT p_page_size
@@ -260,7 +264,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION get_activities_by_following(p_page_num INTEGER, p_page_size INTEGER, p_following_ids UUID[])
+CREATE OR REPLACE FUNCTION as_get_activities_by_following(p_page_num INTEGER, p_page_size INTEGER, p_following_ids UUID[])
     RETURNS TABLE (
         id UUID,
         verb TEXT,
@@ -277,7 +281,7 @@ CREATE OR REPLACE FUNCTION get_activities_by_following(p_page_num INTEGER, p_pag
 BEGIN
     RETURN QUERY
     SELECT a.id, a.verb, a.actor_id, a.object_id, a.target_id, a.latitude, a.longitude, a.location, a."self" , a.created_at, a.updated_at
-    FROM activity a
+    FROM as_activity a
     WHERE a.actor_id = ANY(p_following_ids)
     ORDER BY a.created_at DESC
     LIMIT p_page_size
@@ -286,18 +290,18 @@ END;
 $$ LANGUAGE plpgsql;
 
 ---
-CREATE OR REPLACE FUNCTION get_activities_by_following_as_json(p_page_num INTEGER, p_page_size INTEGER, p_user_id UUID)
+CREATE OR REPLACE FUNCTION as_get_activities_by_following_as_json(p_page_num INTEGER, p_page_size INTEGER, p_user_id UUID)
 RETURNS JSON AS $$
 DECLARE
-    following_ids UUID[] := ARRAY(SELECT get_following_ids(p_user_id));
+    following_ids UUID[] := ARRAY(SELECT as_get_following_ids(p_user_id));
 BEGIN
-    RETURN (SELECT json_agg(self_data) FROM get_activities_by_following(p_page_num, p_page_size, following_ids));
+    RETURN (SELECT json_agg(self_data) FROM as_get_activities_by_following(p_page_num, p_page_size, following_ids));
 END;
 $$ LANGUAGE plpgsql;
 
 ---
 
-CREATE OR REPLACE FUNCTION get_followees_by_object_id(
+CREATE OR REPLACE FUNCTION as_get_followees_by_object_id(
   p_object_id UUID,
   p_page_number INTEGER,
   p_page_size INTEGER
@@ -312,8 +316,8 @@ BEGIN
 
   RETURN QUERY
   SELECT f.followee_id, o.object_data
-  FROM follow f
-  INNER JOIN objectstorage o ON f.followee_id = o.id
+  FROM as_follow f
+  INNER JOIN as_objectstorage o ON f.followee_id = o.id
   WHERE f.follower_id = p_object_id
   ORDER BY o.created_at DESC
   LIMIT p_page_size
@@ -322,14 +326,14 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION get_followees_by_object_id_as_json(
+CREATE OR REPLACE FUNCTION as_get_followees_by_object_id_as_json(
   p_object_id UUID,
   p_page_number INTEGER,
   p_page_size INTEGER
 ) 
 RETURNS JSON AS $$
 BEGIN
-    RETURN (SELECT json_agg(followee_data) FROM get_followees_by_object_id(p_object_id, p_page_number, p_page_size));
+    RETURN (SELECT json_agg(followee_data) FROM as_get_followees_by_object_id(p_object_id, p_page_number, p_page_size));
 END;
 $$ LANGUAGE plpgsql;
 
--- SampleData/SampleDataAndUsageExample.sql
+++ SampleData/SampleDataAndUsageExample.sql
@@ -2,14 +2,14 @@
 
 --create users and activities
 
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     'b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', -- object ID 1
     59.9311, -- latitude
     30.3609, -- longitude
     'user', -- object type
     '{"name": "Alice", "age": 27, "email": "alice@example.com", "picture_url": "https://example.com/pictures/alice.jpg"}' -- object data in JSON format
 );
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     'cc7ebda2-019c-4387-925c-352f7e1f0b10', -- object ID 2
     59.9428, -- latitude
     30.3071, -- longitude
@@ -17,7 +17,7 @@ SELECT upsert_objectstorage(
     '{"name": "Bob", "age": 33, "email": "bob@example.com", "picture_url": "https://example.com/pictures/bob.jpg"}' -- object data in JSON format
 );
 
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     '99875f15-49ee-4e6d-b356-cbab4f4e4a4c', -- object ID 3
     59.9375, -- latitude
     30.3086, -- longitude
@@ -25,7 +25,7 @@ SELECT upsert_objectstorage(
     '{"name": "Charlie", "age": 42, "email": "charlie@example.com", "picture_url": "https://example.com/pictures/charlie.jpg"}' -- object data in JSON format
 );
 
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     '34f6c0a5-5d5e-463f-a2cf-11b7529a92a1', -- object ID 4
     59.9167, -- latitude
     30.25, -- longitude
@@ -33,7 +33,7 @@ SELECT upsert_objectstorage(
     '{"name": "Dave", "age": 29, "email": "dave@example.com", "picture_url": "https://example.com/pictures/dave.jpg"}' -- object data in JSON format
 );
 
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     '8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', -- object ID 5
     59.9391, -- latitude
     30.3158, -- longitude
@@ -44,7 +44,7 @@ SELECT upsert_objectstorage(
 --create ads
 
 -- Bob's ad
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
 'f6c7599e-8161-4d54-82ec-faa13bb8cbf7', -- object ID
 59.9428, -- latitude (near Saint Petersburg)
 30.3071, -- longitude (near Saint Petersburg)
@@ -52,7 +52,7 @@ SELECT upsert_objectstorage(
 '{"description": "Vintage bicycle, good condition", "ad_type": "sale", "picture_url": "https://example.com/pictures/bicycle.jpg"}' -- object data in JSON format
 );
 
-SELECT upsert_activity(
+SELECT as_upsert_activity(
 gen_random_uuid(), -- activity ID
 'post', -- verb
 'cc7ebda2-019c-4387-925c-352f7e1f0b10', -- actor ID (Bob)
@@ -63,7 +63,7 @@ NULL, -- target ID (no target)
 );
 
 -- Charlie's ad
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
 '41f7c558-1cf8-4f2b-b4b4-4d4e4df50843', -- object ID
 59.9375, -- latitude (near Saint Petersburg)
 30.3086, -- longitude (near Saint Petersburg)
@@ -71,7 +71,7 @@ SELECT upsert_objectstorage(
 '{"description": "Smartphone, unlocked", "ad_type": "sale", "picture_url": "https://example.com/pictures/smartphone.jpg"}' -- object data in JSON format
 );
 
-SELECT upsert_activity(
+SELECT as_upsert_activity(
 gen_random_uuid(), -- activity ID
 'post', -- verb
 '99875f15-49ee-4e6d-b356-cbab4f4e4a4c', -- actor ID (Charlie)
@@ -83,7 +83,7 @@ NULL, -- target ID (no target)
 
 
 -- Dave's ad
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
 'c3dd7b47-1bba-4916-8a6a-8b5f2b50ba88', -- object ID
 59.9139, -- latitude (near Saint Petersburg)
 30.3341, -- longitude (near Saint Petersburg)
@@ -91,7 +91,7 @@ SELECT upsert_objectstorage(
 '{"description": "Vintage camera, working condition", "ad_type": "exchange", "picture_url": "https://example.com/pictures/camera.jpg"}' -- object data in JSON format
 );
 
-SELECT upsert_activity(
+SELECT as_upsert_activity(
 gen_random_uuid(), -- activity ID
 'post', -- verb
 '34f6c0a5-5d5e-463f-a2cf-11b7529a92a1', -- actor ID (Dave)
@@ -102,7 +102,7 @@ NULL, -- target ID (no target)
 );
 
 -- Eve's ad
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
 '3453f3c1-296d-47a5-a5a5-f5db5ed3f3b3', -- object ID
 59.9375, -- latitude (near Saint Petersburg)
 30.3141, -- longitude (near Saint Petersburg)
@@ -110,7 +110,7 @@ SELECT upsert_objectstorage(
 '{"description": "Plants, various types", "ad_type": "want", "picture_url": "https://example.com/pictures/plants.jpg"}' -- object data in JSON format
 );
 
-SELECT upsert_activity(
+SELECT as_upsert_activity(
 gen_random_uuid(), -- activity ID
 'post', -- verb
 '8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', -- actor ID (Eve)
@@ -121,7 +121,7 @@ NULL, -- target ID (no target)
 );
 
 -- Alice's ad
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     'b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c02', -- new object ID for Alice's ad
     59.9311, -- latitude
     30.3609, -- longitude
@@ -129,7 +129,7 @@ SELECT upsert_objectstorage(
     '{"description": "Used bicycle, good condition", "ad_type": "sell", "picture_url": "https://example.com/pictures/bicycle.jpg"}' -- ad data in JSON format
 );
 
-SELECT upsert_activity(
+SELECT as_upsert_activity(
     gen_random_uuid(), -- activity ID
     'post', -- verb
     'b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', -- actor ID (Alice)
@@ -139,7 +139,7 @@ SELECT upsert_activity(
     30.3609 -- longitude
 );
 -- Charly's ad 
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     '99875f15-49ee-4e6d-b356-cbab4f4e4a4d', -- new object ID for Charlie's ad
     59.9375, -- latitude
     30.3086, -- longitude
@@ -147,7 +147,7 @@ SELECT upsert_objectstorage(
     '{"description": "Books, various genres", "ad_type": "sell", "picture_url": "https://example.com/pictures/books.jpg"}' -- ad data in JSON format
 );
 
-SELECT upsert_activity(
+SELECT as_upsert_activity(
     gen_random_uuid(), -- activity ID
     'post', -- verb
     '99875f15-49ee-4e6d-b356-cbab4f4e4a4c', -- actor ID (Charlie)
@@ -157,14 +157,14 @@ SELECT upsert_activity(
     30.3071 -- longitude
 );
 -- Bob's ad
-SELECT upsert_objectstorage(
+SELECT as_upsert_objectstorage(
     'cc7ebda2-019c-4387-925c-352f7e1f0b12', -- new object ID for Bob's ad
     59.9428, -- latitude
     30.3071, -- longitude
     'ad', -- object type
     '{"description": "Vintage record player, needs repair", "ad_type": "exchange", "picture_url": "https://example.com/pictures/record_player.jpg"}' -- ad data in JSON format
 );
-SELECT upsert_activity(
+SELECT as_upsert_activity(
     gen_random_uuid(), -- activity ID
     'post', -- verb
     'cc7ebda2-019c-4387-925c-352f7e1f0b10', -- actor ID (Bob)
@@ -179,34 +179,37 @@ SELECT upsert_activity(
 -- Follow data
 
 -- Follow Eve and Alice to themselves
-SELECT follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', '8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54');
-SELECT follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', 'b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01');
+SELECT as_follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', '8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54');
+SELECT as_follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', 'b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01');
 -- Follow Eve and Alice to Bob, Charlie, and Dave
-SELECT follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', 'cc7ebda2-019c-4387-925c-352f7e1f0b10');
-SELECT follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', 'cc7ebda2-019c-4387-925c-352f7e1f0b10');
-SELECT follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', '99875f15-49ee-4e6d-b356-cbab4f4e4a4c');
-SELECT follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', '99875f15-49ee-4e6d-b356-cbab4f4e4a4c');
-SELECT follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', '34f6c0a5-5d5e-463f-a2cf-11b7529a92a1');
-SELECT follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', '34f6c0a5-5d5e-463f-a2cf-11b7529a92a1');
+SELECT as_follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', 'cc7ebda2-019c-4387-925c-352f7e1f0b10');
+SELECT as_follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', 'cc7ebda2-019c-4387-925c-352f7e1f0b10');
+SELECT as_follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', '99875f15-49ee-4e6d-b356-cbab4f4e4a4c');
+SELECT as_follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', '99875f15-49ee-4e6d-b356-cbab4f4e4a4c');
+SELECT as_follow_user('8d7685d5-5b1f-4a7a-835e-b89e7d3a3b54', '34f6c0a5-5d5e-463f-a2cf-11b7529a92a1');
+SELECT as_follow_user('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01', '34f6c0a5-5d5e-463f-a2cf-11b7529a92a1');
 -- Follow data
 
 
 --Examples
 
 --Get the alice followees
-SELECT get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')
+SELECT as_get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')
 
 --Get the activities of alice followees as a table
-SELECT * FROM get_activities_by_following(1, 2, ARRAY(SELECT get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')));
+SELECT * FROM as_get_activities_by_following(1, 2, ARRAY(SELECT as_get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')));
 
 --Get the activities of alice followees as a table and select only the json data of the activity
-SELECT self_data FROM get_activities_by_following(1, 2, ARRAY(SELECT get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')));
+SELECT self_data FROM as_get_activities_by_following(1, 2, ARRAY(SELECT as_get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')));
 
 --Get the activities of alice followees as a table and select only the json data of the activity and create an aggregated json  https://www.postgresql.org/docs/9.5/functions-aggregate.html
-SELECT json_agg(self_data) FROM get_activities_by_following(1, 10, ARRAY(SELECT get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')));
+SELECT json_agg(self_data) FROM as_get_activities_by_following(1, 10, ARRAY(SELECT as_get_following_ids('b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01')));
 
 --Get the activities of alice followees as a json array
-SELECT * FROM get_activities_by_following_as_json(1, 2, 'b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01');
+SELECT * FROM as_get_activities_by_following_as_json(1, 2, 'b8dcbf13-cb01-4a35-93d5-5a5f5a2f6c01');
 
---Will returl all the activities within 4000 radius of the specified lat and lon 
-SELECT get_activities_by_distance_as_json(59.9343, 30.3351, 5000, 1, 10);
+--Will return all the activities within 4000 radius of the specified lat and lon 
+SELECT as_get_activities_by_distance_as_json(59.9343, 30.3351, 5000, 1, 10);
+
+--TODO test
+SELECT * FROM as_get_objects_by_criteria('user', '2022-01-01', NULL, 1, 10);
