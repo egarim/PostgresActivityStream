43ef262b1cead8a59378f99a7b1e2345ad8d9534
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-03-25 16:52:31 +0300

    API Client

---
+++ Clients/DotNet/Ultra.ActivityStream/Ultra.ActivityStream.AspNetCore/Api/Class1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+
+using System.Xml.Linq;
+using Ultra.ActivityStream.Contracts;
+
+namespace Ultra.ActivityStream.AspNetCore.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly ILogger<FilesController> _logger;
+
+        public FilesController(ILogger<FilesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(
+            [FromForm(Name = "Actor")] StreamObject actor,
+            [FromForm(Name = "Object")] StreamObject obj,
+            [FromForm(Name = "Target")] StreamObject target,
+            [FromForm(Name = "Latitude")] double latitude,
+            [FromForm(Name = "Longitude")] double longitude)
+        {
+            try
+            {
+                var files = new List<string>();
+
+                foreach (var formFile in Request.Form.Files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        var filePath = Path.GetTempFileName();
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(stream);
+                        }
+
+                        files.Add(filePath);
+                    }
+                }
+
+                // Do something with the files, actor, obj, target, latitude, and longitude...
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error uploading files.");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+
+}
---
+++ Clients/DotNet/Ultra.ActivityStream/Ultra.ActivityStream.Client/Class1.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using Ultra.ActivityStream.Contracts;
+
+namespace Ultra.ActivityStream.Client
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://example.com/"); // replace with the actual base address of your API
+        }
+
+        public async Task UploadFilesAsync(StreamObject actor, StreamObject obj, StreamObject target, double latitude, double longitude, List<Stream> files)
+        {
+            using (var content = new MultipartFormDataContent())
+            {
+                // Add the actor, obj, target, latitude, and longitude as form data
+                content.Add(new StringContent(JsonConvert.SerializeObject(actor)), "Actor");
+                content.Add(new StringContent(JsonConvert.SerializeObject(obj)), "Object");
+                content.Add(new StringContent(JsonConvert.SerializeObject(target)), "Target");
+                content.Add(new StringContent(latitude.ToString()), "Latitude");
+                content.Add(new StringContent(longitude.ToString()), "Longitude");
+
+                // Add the files as binary data
+                foreach (var file in files)
+                {
+                    var fileContent = new StreamContent(file);
+                    fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                    {
+                        Name = "files",
+                        FileName = Guid.NewGuid().ToString() // generate a unique filename for each file
+                    };
+                    content.Add(fileContent);
+                }
+
+                var response = await _httpClient.PostAsync("files/upload", content);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+
+}
